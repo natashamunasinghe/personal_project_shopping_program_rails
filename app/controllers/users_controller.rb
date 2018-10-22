488194c6d6849380355c55f1cad5328fb95329dd
Your Name <you@example.com>
2018-10-22 16:56:10 +1100

    add crud resource

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+  def update
+  end
+
+  def destroy
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  # get 'users/index'
+  # get 'users/show/:id'
+  # post 'users/create'
+  # get 'users/new'
+  # get 'users/edit'
+  # delete 'users/destroy'
+  # put 'users/update'
+  # patch 'users/update'
+  # delete 'users/destroy'
+
+    get 'users'
+    
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
